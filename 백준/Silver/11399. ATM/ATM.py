450c4c6a442118d3b67a49f4cf123575cec08446
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-09-11 11:39:26 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,11 @@
+people = int(input())
+time = list(map(int, input().split()))
+tTime = [0]*people
+
+time.sort()
+total=0
+
+for i in range(0, people):
+    total = total + time[i]
+    tTime[i]=total
+print(sum(tTime))
