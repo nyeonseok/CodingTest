31e9d69b51479a9ac3b71d5c56f564ce636ae39a
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-09-14 02:21:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,9 @@
+input_text = input()
+ascii_value = ord(input_text)
+
+if 'a' <= input_text <= 'z':
+    print(ascii_value)
+elif 'A' <= input_text <= 'Z':
+    print(ascii_value)
+else:
+    print(ascii_value)
