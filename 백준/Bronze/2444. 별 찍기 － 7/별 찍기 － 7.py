a9eb88e3e10a1b2ddab05474d7ae764f0a95cc8e
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-09-14 20:34:28 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -4,6 +4,5 @@ time = 2*n-1
 for i in range(1, time+1):
     if i < n:
         print(" "*(n-i) + "*"*(2*i-1))
-    elif i==n: print("*"*(2*n-1))
     else:
         print(" "*(i-n) + "*"*(2*(2*n-i)-1))
