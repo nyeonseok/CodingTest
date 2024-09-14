10789ebb928de5e8ec7a9b42cfecc3a0745156d2
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-09-14 19:14:55 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,25 +1,16 @@
-alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# alpha = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], [
-#     'M', 'N', 'O'], ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
+alpha = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], [
+    'M', 'N', 'O'], ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
 
 time = 0
 
 input_alpha = list(input())
 
 
-
 for i in range(len(input_alpha)):
-    if 'A' <= input_alpha[i] <= 'C': time +=3
-    elif 'D' <= input_alpha[i] <= 'F': time +=4
-    elif 'G' <= input_alpha[i] <= 'I': time +=5
-    elif 'J' <= input_alpha[i] <= 'L': time +=6
-    elif 'M' <= input_alpha[i] <= 'O': time +=7
-    elif 'P' <= input_alpha[i] <= 'S': time +=8
-    elif 'T' <= input_alpha[i] <= 'V': time +=9
-    else: time +=10
+    for j in range(len(alpha)):
+        if input_alpha[i] in alpha[j]:
+            time += j+3
 
-
-    
 
 
 print(time)
