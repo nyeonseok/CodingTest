3aa462a6af3d339091e5b85ce68f06c4326b03ca
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-09-15 20:19:50 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+a = [[]*15 for _ in range(5)]
+
+for i in range(5):
+    a[i] = list(input())
+
+for i in range(15):
+    for j in range(5):
+        if i < len(a[j]):
+            print(a[j][i], end='')
+        else: continue
