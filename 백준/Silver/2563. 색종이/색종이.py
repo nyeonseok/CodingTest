7952c2bb80486512f4c440de6ba66daa1fa858d9
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-11-06 12:45:15 +0900

    [Silver V] Title: 색종이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,5 +1,5 @@
 import sys
-ground = [[0]*100 for _ in range(100)]
+ground = [[False]*100 for _ in range(100)]
 colors = []
 
 n = int(sys.stdin.readline().strip())
@@ -13,8 +13,8 @@ for _ in range(n):
 def search(ground, arr):
     for i in range(arr[0], arr[0]+10):
         for j in range(arr[1], arr[1]+10):
-            if ground[i][j] == 0:
-                ground[i][j] = 1
+            if ground[i][j] == False:
+                ground[i][j] = True
             else:
                 continue
     return ground
@@ -24,6 +24,6 @@ for color in colors:
     ground = search(ground, color)
 
 cnt = 0
-cnt = sum(1 for i in range(100) for j in range(100) if ground[i][j] == 1)
+cnt = sum(1 for i in range(100) for j in range(100) if ground[i][j] == True)
 
 print(cnt)
